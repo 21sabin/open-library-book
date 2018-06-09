4602cf52b26059b1d4ad75c707ae8253d8c0485f
Saigal <saigal.amatya@gmail.com>
2018-06-09 06:19:14 +0545

    logout added

--- src/app/components/common/header.component.ts
+++ src/app/components/common/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthenticationService } from '../../services';
 
 
 
@@ -10,12 +11,16 @@ import { NgForm } from '@angular/forms';
      
 })
 export class HeaderComponent {
-  constructor(){}
+  constructor(
+    private authService: AuthenticationService
+  ){}
 
-  
-    
   ngOnInit(){
    
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 
